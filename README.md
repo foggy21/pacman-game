901ee29051aaaee934c45d2eaf3fc38da7803d0d
foggy21 <rashit.xaliullin@inbox.ru>
2023-03-01 21:44:31 +1000

    Made reading and creating map

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Learning of threading in C#
 <p style="font-size:17.5px">Tasks:</p>
 
-1. Create map from file.
+1. Read map from file.
 2. Draw map and entities.
 3. Divide threads to movement of player happened separately from movement of enemies.
 4. Pacman (player) have to eat points (.) to get score.
---
+++ pacman-game/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char[,] map = ReadMap("map.txt");
+
+        DrawMap(map);
+    }
+
+    private static void DrawMap(char[,] map)
+    {
+        for (int y = 0; y < map.GetLength(1); ++y)
+        {
+            for (int x = 0; x < map.GetLength(0); ++x)
+                Console.Write(map[x, y]);
+            Console.Write('\n');
+        }
+    }
+
+    private static char[,] ReadMap(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+        char[,] map = new char[GetMaxLengthOfRow(lines), lines.Length];
+
+        for (int y = 0; y < map.GetLength(1); ++y)
+            for (int x = 0; x < map.GetLength(0); ++x)
+                map[x, y] = lines[y][x];
+
+        return map;
+    }
+
+    private static int GetMaxLengthOfRow(string[] rows)
+    {
+        int maxLength = rows[0].Length;
+        foreach (string row in rows)
+        {
+            if (row.Length > maxLength) 
+                maxLength = row.Length;
+        }
+        return maxLength;
+    }
+}
